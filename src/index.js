86800cebcb366852b715fbc69e0fa02d46d3f688
azarovalex <alex@azarov.by>
2018-03-01 02:38:09 +0300

    Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackArr = str.split('');
+	let stack = [];
+  stack.push(brackArr[0]);
+  
+  for (var i = 1; i < brackArr.length; i++) {
+    let prevBrack = bracketsConfig.find(function(brackPair) {
+      return stack[stack.length - 1] == brackPair[0];
+    });
+    
+    if (prevBrack != undefined)
+      prevBrack = prevBrack[1];
+    
+    if (prevBrack == brackArr[i]) {
+      stack.pop();
+    } else {
+      stack.push(brackArr[i]);
+    }
+    
+  }
+  
+  return stack.length == 0;
 }
